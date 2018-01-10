1579fa42eb3e81423ce8fc962eb6bac0afa1d471
Mike Lazer-Walker <michael@lazerwalker.com>
2018-01-10 15:31:30 -0500

    Tidy some stuff up into functions

--- game.js
+++ game.js
@@ -1,52 +1,59 @@
 var canvas = document.getElementById("game");
 var player = document.getElementById("player");
 
-var canvasStyle = window.getComputedStyle(canvas)
-var playerStyle = window.getComputedStyle(player)
+var position, playerOffset;
 
-// Set start position / offset
-var playerOffset = {
-  x: (parseInt(canvasStyle.width) - parseInt(playerStyle.width)) / 2,
-  y: (parseInt(canvasStyle.height) - parseInt(playerStyle.height)) / 2
-};
-
-var position = {
-  x: 0,
-  y: 0
-};
-
-// Set up keyboard event handlers
-var Directions = [
+var directions = [
   { keyCode: 37,  name: "left", x: -0.25, y: 0 },
   { keyCode: 39, name: "right", x: 0.25, y: 0 },
   { keyCode: 38, name: "up", x: 0, y: -0.25 },
   { keyCode: 40, name: "down", x: 0, y: 0.25 }
 ]
 
-window.addEventListener('keyup', function(e) {
-  var direction = Directions.filter(function(d) {
-    return d.keyCode === e.keyCode
-  })[0];
+setupStartingPosition();
+setupKeyboardHandling();
 
-  if (direction) {
-    direction.pressed = false;
-  }
-});
 
-window.addEventListener('keydown',  function(e) {
-  console.log(e.keyCode)
+function setupStartingPosition() {
+  var canvasStyle = window.getComputedStyle(canvas)
+  var playerStyle = window.getComputedStyle(player)
 
-  var direction = Directions.filter(function(d) {
-    console.log(d.keyCode)
-    return d.keyCode === e.keyCode
-  })[0];
+  playerOffset = {
+    x: (parseInt(canvasStyle.width) - parseInt(playerStyle.width)) / 2,
+    y: (parseInt(canvasStyle.height) - parseInt(playerStyle.height)) / 2
+  };
 
-  if (direction) {
-    console.log("Pressed " + direction.name)
-    direction.pressed = true;
-  }
-});
+  position = {
+    x: 0,
+    y: 0
+  };
+}
 
+function setupKeyboardHandling() {
+  window.addEventListener('keyup', function(e) {
+    var direction = Directions.filter(function(d) {
+      return d.keyCode === e.keyCode
+    })[0];
+
+    if (direction) {
+      direction.pressed = false;
+    }
+  });
+
+  window.addEventListener('keydown',  function(e) {
+    console.log(e.keyCode)
+
+    var direction = Directions.filter(function(d) {
+      console.log(d.keyCode)
+      return d.keyCode === e.keyCode
+    })[0];
+
+    if (direction) {
+      console.log("Pressed " + direction.name)
+      direction.pressed = true;
+    }
+  });
+}
 
 // Render the new position
 function setPosition(pos, image) {
@@ -58,7 +65,6 @@ function setPosition(pos, image) {
   var stringPos = newPos.x + " " + newPos.y;
   image.style.backgroundPosition = stringPos;
 }
-
 
 // Main game loop
 setInterval(function() {
